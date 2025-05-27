f90f3001c8b3d591b7e3374d5db89fbd29b9b356
Abhishek <abhi.jkit@gmail.com>
2025-05-28 00:48:33 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import {app} from "./app.js"
 dotenv.config({
     path:'./env'
 })
-
+const PORT = process.env.PORT || 8000;
 dbConnect()
 .then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
+    app.listen(PORT, '0.0.0.0', ()=>{
         console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })

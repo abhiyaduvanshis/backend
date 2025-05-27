9183d5bca96eff8e23a46ce1e6a76d4c67aa83cf
Abhishek <abhi.jkit@gmail.com>
2025-05-27 17:56:00 +0530

    changes

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+const dbConnect = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log('db connected succesfully!')
+    } catch (error) {
+        console.log('Mongodb connection error',error)
+        process.exit(1)
+    }
+}
+
+export default dbConnect

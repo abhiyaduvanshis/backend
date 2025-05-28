5ded55906dcce53b4a326b5eeb967592c09edf56
Abhishek <abhi.jkit@gmail.com>
2025-05-29 00:05:12 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app = express()
-app.options('*', cors());
-app.use(cors())
+
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials: true
+}))
 
 
 app.use(express.json({limit: "16kb"}))

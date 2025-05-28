3de2f0927828d95194638393e527b526b10f59b8
Abhishek <abhi.jkit@gmail.com>
2025-05-29 00:23:33 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'http://127.0.0.1:3000',
     credentials: true
 }))
 

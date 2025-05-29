74b215199b3d6fb47d11c8735537c790b4ba974a
Abhishek <abhi.jkit@gmail.com>
2025-05-29 14:24:17 +0530

    changes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -12,7 +12,7 @@ export const verifyJWT = asyncHandler(async(req,res,next)=>{
          console.log(token)
         console.log(process.env.ACCESS_TOKEN_SECRET)
         const decodeToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-        console.log(decodeToken)
+        console.log(decodeToken,'tokede')
         const user = await User.findById(decodeToken._id)
         console.log(user)
         if (!user) {

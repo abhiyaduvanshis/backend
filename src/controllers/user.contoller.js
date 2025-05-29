2b5a6d29cb5ef35dbe665d101d357168fb8df510
Abhishek <abhi.jkit@gmail.com>
2025-05-29 14:02:33 +0530

    changes

--- src/controllers/user.contoller.js
+++ src/controllers/user.contoller.js
@@ -139,9 +139,7 @@ const userLogin = asyncHandler( async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: false,
-        sameSite: 'none',
-        maxAge: 24 * 60 * 60 * 1000,
+        secure: true,
     }
 
 
